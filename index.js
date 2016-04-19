7973df43749ccba467e4db6b34464e58e5b5553c
alphapilgrim <timothy_lombrana@yahoo.com>
2016-04-19 13:56:27 -0500

    4-19-2016
    fix - uglification of files.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-require('./scripts/Controllers')(app);
-require('./scripts/Directive')(app);
-require('./scripts/Services')(app);
-require('./scripts/filters')(app);
+// require('./scripts/Controllers')(app);
+// require('./scripts/Directive')(app);
+// require('./scripts/Services')(app);
+// require('./scripts/filters')(app);
 // Load the core build.
-var _ = require('lodash/core');
+// var _ = require('lodash/core');
+
+// Commented out webpack for distribution testing.
