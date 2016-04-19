d38cf83f873ff68dbb94d3ab8e667c4b2cbc69ca
alphapilgrim <timothy_lombrana@yahoo.com>
2016-04-19 11:35:20 -0500

    Revert "Merge branch 'master' of https://github.com/alphapilgrim/alphapilgrim.github.io"
    This reverts commit 9e2d7c40a11d997b51ea93899c1d80e10966a103, reversing
    changes made to 363bcdc68b58114a3e19447abdad6aa0b5ac7e09.

--- index.js
+++
@@ -1,6 +0,0 @@
-require('./scripts/Controllers')(app);
-require('./scripts/Directive')(app);
-require('./scripts/Services')(app);
-require('./scripts/filters')(app);
-// Load the core build.
-var _ = require('lodash/core');
