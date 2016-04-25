ac9a69ef480230f67d71266987afbc5a12b5c9b3
alphapilgrim <Tim@alphapilgrim.local>
2016-04-25 10:59:47 -0500

    Revert "remove test files"
    This reverts commit 960dca49b267a954de603ea4f97dff17a381b0e8.

---
+++ index.js
@@ -0,0 +1,6 @@
+require('./scripts/Controllers')(app);
+require('./scripts/Directives')(app);
+require('./scripts/Services')(app);
+require('./scripts/filters')(app);
+
+// // comment out above if this doesnt work.
