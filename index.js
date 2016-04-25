960dca49b267a954de603ea4f97dff17a381b0e8
alphapilgrim <Tim@alphapilgrim.local>
2016-04-25 10:58:58 -0500

    remove test files

--- index.js
+++
@@ -1,6 +0,0 @@
-require('./scripts/Controllers')(app);
-require('./scripts/Directives')(app);
-require('./scripts/Services')(app);
-require('./scripts/filters')(app);
-
-// // comment out above if this doesnt work.
